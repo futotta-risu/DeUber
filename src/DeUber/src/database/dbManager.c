ecbd3b5ae57ac41e78833971832880df10d4b4f3
Aritz Zugazaga <aritzugazaga@opendeusto.es>
2020-02-24 18:10:25 +0100

    Clase DBManager acabada

--- src/DeUber/src/database/dbManager.c
+++ src/DeUber/src/database/dbManager.c
@@ -30,7 +30,7 @@ int connectDB() {
     sqlite3_close(db);
 }
 
-int createTableDB() {
+int createTablePersona() {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
@@ -47,12 +47,44 @@ int createTableDB() {
     }
 
     /* Create SQL statement */
-    sql = "CREATE TABLE  IF NOT EXIST CAR("  \
-      "ID INT PRIMARY KEY     NOT NULL," \
-      "NAME           TEXT    NOT NULL," \
-      "AGE            INT     NOT NULL," \
-      "ADDRESS        CHAR(50)," \
-      "SALARY         REAL );";
+    sql = "CREATE TABLE  IF NOT EXIST PERSONA("  \
+      "ID_PERSONA INT PRIMARY KEY     NOT NULL," \
+      "NOMBRE           TEXT    ," \
+      "RUTA            INT     );";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if( rc != SQLITE_OK ){
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
+    sqlite3_close(db);
+    return 0;
+}
+
+int createTableMapa() {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+
+    /* Open database */
+    rc = sqlite3_open("database.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    sql = "CREATE TABLE  IF NOT EXIST MAPA("  \
+      "ID_MAPA INT PRIMARY KEY     NOT NULL," \
+      "NOMBRE           TEXT    );";
 
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
@@ -84,14 +116,14 @@ int insertDB() {
     }
 
     /* Create SQL statement */
-    sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+    sql = "INSERT INTO PERSONA (ID_PERSONA,NOMBRE,RUTA) "  \
+         "VALUES (1, 'Paul', 32); " \
+         "INSERT INTO PERSONA (ID_PERSONA,NOMBRE,RUTA) "  \
+         "VALUES (2, 'Allen', 25); "     \
+         "INSERT INTO MAPA (ID_MAPA,NOMBRE)" \
+         "VALUES (1, 'Bilbao');" \
+         "INSERT INTO MAPA (ID_MAPA,NOMBRE)" \
+         "VALUES (2, 'Madrid');";
 
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
@@ -106,7 +138,7 @@ int insertDB() {
     return 0;
 }
 
-int selectDB() {
+int selectPersona() {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
@@ -124,7 +156,7 @@ int selectDB() {
     }
 
     /* Create SQL statement */
-    sql = "SELECT * from COMPANY";
+    sql = "SELECT * from PERSONA";
 
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
@@ -139,7 +171,40 @@ int selectDB() {
     return 0;
 }
 
-int updateDB() {
+int selectMapa() {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+    const char* data = "Callback function called";
+
+    /* Open database */
+    rc = sqlite3_open("database.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    sql = "SELECT * from MAPA";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sqlite3_close(db);
+    return 0;
+}
+
+int updatePersona() {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
@@ -157,8 +222,8 @@ int updateDB() {
     }
 
     /* Create merged SQL statement */
-    sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1; " \
-         "SELECT * from COMPANY";
+    sql = "UPDATE PERSONA set RUTA = 14 where ID=1; " \
+         "SELECT * from PERSONA";
 
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
@@ -172,7 +237,8 @@ int updateDB() {
     sqlite3_close(db);
     return 0;
 }
-int deleteFromBD() {
+
+int updateMapa() {
     sqlite3 *db;
     char *zErrMsg = 0;
     int rc;
@@ -190,8 +256,75 @@ int deleteFromBD() {
     }
 
     /* Create merged SQL statement */
-    sql = "DELETE from COMPANY where ID=2; " \
-         "SELECT * from COMPANY";
+    sql = "UPDATE MAPA set NOMBRE = 'Barcelona' where ID=2; " \
+         "SELECT * from MAPA";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sqlite3_close(db);
+    return 0;
+}
+
+int deleteFromPersona(int id) {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+    const char* data = "Callback function called";
+
+    /* Open database */
+    rc = sqlite3_open("database.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    /* Create merged SQL statement */
+    sql = "DELETE from PERSONA where ID= " + id + "; " \
+         "SELECT * from PERSONA";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sqlite3_close(db);
+    return 0;
+}
+int deleteFromMapa(int id) {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+    const char* data = "Callback function called";
+
+    /* Open database */
+    rc = sqlite3_open("database.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    /* Create merged SQL statement */
+    sql = "DELETE from MAPA where ID= " + id + "; " \
+         "SELECT * from MAPA";
 
     /* Execute SQL statement */
     rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
