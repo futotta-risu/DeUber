eca4a9840fa9a988910597d9f3d3503a615cd2b4
Erik Terres <37312201+erikberter@users.noreply.github.com>
2020-02-26 10:22:46 +0100

    Movido el proyecto una capa al exterior

---
+++ src/global.h
@@ -0,0 +1,13 @@
+//
+// Created by whiwho on 25/02/2020.
+//
+
+#ifndef DEUBER_GLOBAL_H
+#define DEUBER_GLOBAL_H
+
+static const char config_file[] = "../data/config";
+static const char menu_file[] = "../data/menu.dg";
+static const char map_file[] = "../data/maps/ej1.map";
+static const char map_write[] = "../data/maps/mapaWrite.map";
+
+#endif //DEUBER_GLOBAL_H
